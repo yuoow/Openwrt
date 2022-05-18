ae50f00dfbfe134dd49d35dd9efe6ddf837d5535
yumoon <yumoon@live.com>
2022-05-18 16:48:48 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,27 +11,38 @@
 #
 
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-# 1.更改默认IP
+# 更改默认IP
 sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
 
-# 2.清除默认密码/改密码为 
+# 清除默认密码/改密码为 
 sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz-default-settings
 
-# 5.修改默认主题
+# 修改默认主题
 sed -i 's/luci-theme-bootstrap/luci-theme-argonne/g' feeds/luci/collections/luci/Makefile
 
-# 6.修改机器名称
+# 修改机器名称
 sed -i 's/OpenWrt/YuMoon/g' package/base-files/files/bin/config_generate
 
-# 7.修改wifi名称
+# 修改wifi名称
 sed -i 's/OpenWrt/YuMoon/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
-# 8.修改banner
+# 修改banner
 rm -rf package/base-files/files/etc/banner
 cp -f ../banner package/base-files/files/etc/
 
-# echo '删除旧版argon,链接新版'
-# rm -rf ./package/lean/luci-theme-argon
-# ln -s ../../../luci-theme-argon ./package/lean/
+# 修改时区'
+sed -i "s/'UTC'/'CST-8'\n        set system.@system[-1].zonename='Asia\/Shanghai'/g" package/base-files/files/bin/config_generate
 
 
+cat > package/base-files/files/etc/banner <<EOF
+_____.___.        _____                        
+\__  |   |__ __  /     \   ____   ____   ____  
+ /   |   |  |  \/  \ /  \ /  _ \ /  _ \ /    \ 
+ \____   |  |  /    Y    (  <_> |  <_> )   |  \
+ / ______|____/\____|__  /\____/ \____/|___|  /
+ \/    W I R E L E S S   \/  F R E E D O M  \/                            
+ -----------------------------------------------------
+ CHAOS CALMER (%C, %R) Ver:0.1-2022-05-18 by YuMoon
+ -----------------------------------------------------
+EOF
+cp $GITHUB_WORKSPACE/banner package/base-files/files/etc/banner
