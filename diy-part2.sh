cfa185a940eeaf2e5d2037f9e036a7aa0b9df4e9
yumoon <yumoon@live.com>
2022-05-23 15:34:38 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -26,10 +26,6 @@ sed -i 's/OpenWrt/YuMoon/g' package/base-files/files/bin/config_generate
 # 修改wifi名称
 sed -i 's/OpenWrt/YuMoon/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
-# 修改banner
-# rm -rf package/base-files/files/etc/banner
-# cp -f ../banner package/base-files/files/etc/
-
 # 修改时区'
 sed -i "s/'UTC'/'CST-8'\n        set system.@system[-1].zonename='Asia\/Shanghai'/g" package/base-files/files/bin/config_generate
 
