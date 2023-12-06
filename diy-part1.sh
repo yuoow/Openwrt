a7b6302c1896e47847cb56dfc72f654ada99c666
yumoon <yumoon@live.com>
2023-12-06 11:35:13 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -86,7 +86,7 @@ git clone https://github.com/pymumu/luci-app-smartdns.git package/applications/l
 # NAS
 
 #文件助手
-svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-fileassistant package/luci-app-fileassistant
+git clone https://github.com/kiddin9/openwrt-packages/tree/master/luci-app-fileassistant.git package/luci-app-fileassistant
 
 ##############################################################################################
 
