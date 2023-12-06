5d8593b42d6e21b0f53df9df3e4e2dc68ce44086
yumoon <yumoon@live.com>
2023-12-06 11:33:11 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -80,7 +80,7 @@ git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci
 
 #luci-app-smartdns
 
-#git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/applications/luci-app-smartdns
+git clone https://github.com/pymumu/luci-app-smartdns.git package/applications/luci-app-smartdns
 ##############################################################################################
 
 # NAS
